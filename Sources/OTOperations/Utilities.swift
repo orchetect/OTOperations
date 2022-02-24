4cb703de6bef9da3bb2a754d0381d5827ad6f36f
Steffan Andrews <stef@orchetect.com>
2022-02-24 10:05:38 -0800

    `LabelProgress`: Refactored to no longer depend on `userInfo` dictionary

---
+++ Sources/OTOperations/Utilities.swift
@@ -0,0 +1,38 @@
+//
+//  Utilities.swift
+//  OTOperations • https://github.com/orchetect/OTOperations
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+extension Collection where Element : Equatable {
+    
+    /// Returns a new array removing duplicate elements, maintaining original array order.
+    @_disfavoredOverload
+    internal func removingDuplicates() -> [Element] {
+        
+        reduce(into: []) { accum, element in
+            guard !accum.contains(element) else { return }
+            accum.append(element)
+        }
+        
+    }
+    
+}
+
+extension Collection where Element : Hashable,
+                           Element : Comparable {
+    
+    /// Returns a new array removing duplicate elements, sorting results by default sort method.
+    @_disfavoredOverload
+    internal func removingDuplicatesSorted() -> [Element] {
+        
+        Set(self).sorted()
+        
+    }
+    
+}
+
+#endif
