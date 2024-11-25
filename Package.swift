716fbc523390efdf2bf6cd1a78fbb23bd4ad0513
Steffan Andrews <stef@orchetect.com>
2024-11-25 03:00:19 -0800

    Bumped package minimum requirements to macOS 10.15, iOS 13, tvOS 13, watchOS 6

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "OTOperations",
     
     platforms: [
-        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     
     products: [
